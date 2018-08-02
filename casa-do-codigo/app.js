56113c8110ad7dc17cfda3579f4a2087d46916b3
HemersonGH <hemersonel@gmail.com>
2018-08-01 22:44:50 -0300

    Inserindo WebSocket Client-Server

--- casa-do-codigo/app.js
+++ casa-do-codigo/app.js
@@ -1,6 +1,10 @@
 var app = require('./config/express')();
 // var routeProdutos = require('./app/routes/produtos')(app); // load already did it
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.listen(3000, function() {
+app.set('io', io);
+
+http.listen(3000, function() {
   console.log("Server running");
 });
--- casa-do-codigo/app/routes/promocoes.js
+++ casa-do-codigo/app/routes/promocoes.js
@@ -1,16 +1,17 @@
 module.exports = function(app) {
-    app.get("/promocoes/form", function(req, res) {
+    app.get("/promocoes/form", function(request, responce) {
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
-        produtosDAO.lista(function(erros, resultados) {
-            res.render('promocoes/form', {lista: resultados});
+        produtosDAO.lista(function(erros, responceultados) {
+            responce.render('promocoes/form', {lista: responceultados});
         });
         connection.end();
     });
 
-    app.post("/promocoes", function(req, res) {
-        var promocao = req.body;
+    app.post("/promocoes", function(request, responce) {
+        var promocao = request.body;
+        console.log();
         app.get('io').emit('novaPromocao', promocao);
-        res.redirect('promocoes/form');
+        responce.redirect('promocoes/form');
     });
 }
