3c6818bd4464353fc4479511f4af3207a39bd493
HemersonGH <hemersonel@gmail.com>
2018-08-07 00:29:57 -0300

    Fazeno=do deploy

--- casa-do-codigo/app.js
+++ casa-do-codigo/app.js
@@ -2,10 +2,9 @@ var app = require('./config/express')();
 // var routeProdutos = require('./app/routes/produtos')(app); // load already did it
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.set('io', io);
-
-var port = process.env.PORT || 3000;
 
 http.listen(port, function() {
   console.log("Server running at HEROKU");
